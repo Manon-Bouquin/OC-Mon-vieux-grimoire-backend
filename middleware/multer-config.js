0c6eda73c66e4f207edd4c98b465bed7d8da27eb
Manon Bouquin <Manon.bouquin@hotmail.fr>
2024-10-23 16:47:44 +0200

    Ajout de bestRating + Compression d'image

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,23 +1,6 @@
 //npm install --save multer
 const multer = require("multer");
 
-const MIMES_TYPES = {
-    "image/jpg": "jpg",
-    "image/jpeg": "jpg",
-    "image/png": "png",
-    "image/webp": "webp"
-};
-
-const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, "images")
-    },
-    filename: (req, file, callback) => {
-        const name = file.originalname.split(" ").join("_");
-        //remplace les espaces des noms des fichiers par un _ 
-        const extension = MIMES_TYPES[file.mimetype];
-        callback(null, name + Date.now() + "." + extension);
-    }
-});
+const storage = multer.memoryStorage();
 
 module.exports = multer({ storage }).single("image");
--- routes/book.js
+++ routes/book.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
-const bookCtrl = require ("../controllers/book");
+const bookCtrl = require("../controllers/book");
+const compressImage = require("../middleware/sharp");
 
 
-router.get("/", bookCtrl.getAllBooks);
-router.get("/:id", bookCtrl.getOneBook);
-router.post("/", auth, multer, bookCtrl.createBook);
+router.get("/bestrating", bookCtrl.bestRating); //Route spécifique d'abord
+router.get("/", bookCtrl.getAllBooks); 
+router.get("/:id", bookCtrl.getOneBook); //Route dynamique ensuite
+router.post("/", auth, multer, compressImage, bookCtrl.createBook);
 router.post("/:id/rating", auth, bookCtrl.ratingBook);
-router.put("/:id", auth, multer, bookCtrl.modifyBook);
+router.put("/:id", auth, multer, compressImage, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook)
 
 module.exports = router;
